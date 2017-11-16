be02d3e75220ad1e135ea31aaa9d9ef86d113f08
puppert <857515852@qq.com>
2017-11-16 14:50:45 +0800

    20171116

--- src/compiler/grammatical_analysis/constructor/ProjectConstructor.java
+++ src/compiler/grammatical_analysis/constructor/ProjectConstructor.java
@@ -5,38 +5,77 @@ import java.util.List;
 
 import compiler.grammatical_analysis.pojo.Connect;
 import compiler.grammatical_analysis.pojo.Project;
+import compiler.grammatical_analysis.pojo.Rule;
 import compiler.grammatical_analysis.pojo.Rule_pointer;
+import compiler.pojo.word.Word;
+import utils.Utils_properties;
 
 public class ProjectConstructor {
 	public static List<Project> plist = null;
 	public static List<Connect> clist = ConnectConstructor.getClist();
+	public static List<Rule> rlist = Utils_properties.getRules();
 	
 	public void initialize() {
 		plist = new ArrayList<>();
+		Project p = new Project();
+		p.setStatus("I0");
+		List<Rule_pointer> rplist = new ArrayList<>();
+		for(Rule r:rlist) {
+			Rule_pointer rp = new Rule_pointer();
+			rp.setLeft(r.getLeft());
+			rp.setRight(r.getRight());
+			rp.setPointer(r.getRight().get(0).getWord());
+			rp.setSymbol("#");
+			rplist.add(rp);
+		}
+		p.setRules(rplist);
+		plist.add(p);
 	}
 	
 	public Project CreateNextProject(Connect c) {
 		Project p = new Project();
 		p.setStatus(c.getFollow());
-		List<Rule_pointer> list = new ArrayList<>();
+		List<Rule_pointer> folist = new ArrayList<>();
 		for(Project pro:plist) {
 			if(pro.getStatus().equals(c.getFront())) {
 				Project front = pro;
-				List<Rule_pointer> rlist = front.getRules();
-				for(Rule_pointer r:rlist) {
+				List<Rule_pointer> frlist = front.getRules();
+				for(Rule_pointer r:frlist) {
 					if(r.getPointer().equals(c.getSign())) {
 						RuleConstructor rc = new RuleConstructor();
 						Rule_pointer newr = rc.ChangeSign(r, r.getPointer());
-						list.add(newr);
-						
+						folist.add(newr);
 					}
 				}
-				/*
-				 * 补齐整个项目
-				 * 
-				 * 
-				 * */
 			}
+		}
+		p.setRules(folist);
+		p = CompliteProject(p);
+		return p;
+	}
+	
+	private Project CompliteProject(Project p) {
+		List<Rule_pointer> list = p.getRules();
+		List<Rule> rlist = Utils_properties.getRules();
+		boolean change = false;
+		for(Rule_pointer l:list) {
+			String point = l.getPointer();
+			for(Rule r:rlist) {
+				if(point.equals(r.getLeft())) {
+					List<Word> rl = r.getRight();
+					Rule_pointer rp = new Rule_pointer();
+					rp.setLeft(r.getLeft());
+					rp.setRight(rl);
+					rp.setPointer(rl.get(0).getWord());
+					if(!list.contains(rp)) {
+						list.add(rp);
+						change = true;
+					}
+				}
+			}
+		}
+		if(change) {
+			p = CompliteProject(p);
 		}
 		return p;
 	}
--- src/utils/Utils_properties.java
+++ src/utils/Utils_properties.java
@@ -1,9 +1,20 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import compiler.grammatical_analysis.pojo.Rule;
+import compiler.lexical_analysis.Analysis;
+import compiler.pojo.word.Word;
+import compiler.pojo.word.Vn.Vn_word;
 
 /**
  * @author Puppert
@@ -37,4 +48,46 @@ public class Utils_properties {
 		} 
 		
 	}
+	
+	private static List<Rule> grammer_pro = new ArrayList<>();
+	
+	public static List<Rule> getRules(){
+		if(grammer_pro.isEmpty()) {
+			loadRule();
+		}
+		return grammer_pro;
+	}
+	
+	private static void loadRule() {
+		Analysis a = new Analysis();
+		try {
+			 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("rule.txt"))));
+			 String line = null;
+			 while((line = br.readLine()) != null) {
+				 Rule r = new Rule();
+				 String[] str = line.split("->");
+				 Vn_word left = new Vn_word();
+				 left.setWord(str[0]);
+				 r.setLeft(left);
+				 Map<String, String> m = a.getAllContants(str[1]);
+				 Map<String, String> resultMap = Myutils.sortMapByKey(m); 
+				 List<Word> list = new ArrayList<>();
+				 for(Map.Entry<String, String> entry : resultMap.entrySet()) {
+					 Word w = new Word();
+					 w.setWord(entry.getValue());
+					 list.add(w);
+				 }
+				 r.setRight(list);
+				 grammer_pro.add(r);
+			 }
+			 br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
